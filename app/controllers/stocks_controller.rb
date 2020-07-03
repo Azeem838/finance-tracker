d2f63f640afb33f82fa6076bc4baf89275ada578
Azeem <azeemahmed101@gmail.com>
2020-07-03 16:53:58 +0200

    update flash messages to work with javascript

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,12 +9,16 @@ class StocksController < ApplicationController
           format.js { render partial: 'users/result' }
         end
       else
-        flash[:alert] = 'Please enter a valid symbol to search'
-        render 'users/my_portfolio'
+        respond_to do |format|
+          flash.now[:alert] = 'Please enter a valid symbol to search'
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = 'Please enter a symbol to search'
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a symbol to search'
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 end
