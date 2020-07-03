69f6d4c1f00f012d3ddddae79402d82419540c2f
Azeem <azeemahmed101@gmail.com>
2020-07-03 18:46:32 +0200

    add functionality to remove tracking of stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: %i[create destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
