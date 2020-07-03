efeeb4c29130f8468877f791a764459b8baaefe4
Azeem <azeemahmed101@gmail.com>
2020-07-03 17:53:28 +0200

    add functionality to track stocks from front end

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,6 @@ class Stock < ApplicationRecord
 
   validates :name, :ticker, presence: true
 
-
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
@@ -15,5 +14,9 @@ class Stock < ApplicationRecord
     rescue StandardError => e
       nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
