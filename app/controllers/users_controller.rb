c075d20a0007dae812f42d0ffe1ad81af673daee
Azeem <azeemahmed101@gmail.com>
2020-07-04 12:16:33 +0200

    add ajax search friends and js response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a friend name or email to search'
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
 end
