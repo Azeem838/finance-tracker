af305c8998e4f8af59e305c967cc5d04b403d56e
Azeem <azeemahmed101@gmail.com>
2020-07-04 14:02:42 +0200

    implent add friend functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class FriendshipsController < ApplicationController
-  def create; end
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = 'Following friend'
+    else
+      flash[:alert] = 'There was something wrong wtih the tracking request'
+    end
+    redirect_to my_friends_path
+  end
 
   def destroy
     friendship = current_user.friendships.where(friend_id: params[:id]).first
