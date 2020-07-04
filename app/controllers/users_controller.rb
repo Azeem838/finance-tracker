940b820f0407655f6250105c93105da47abc692a
Azeem <azeemahmed101@gmail.com>
2020-07-04 11:34:25 +0200

    add my friends view with list of following friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def my_friends
+    @friends = current_user.friends
   end
 end
