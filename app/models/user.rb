eafe8d2756f94a1e09e1f366476c72146c832e94
Azeem <azeemahmed101@gmail.com>
2020-07-04 13:43:59 +0200

    display search results in browser

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,4 +54,8 @@ class User < ApplicationRecord
   def self.matchers(field_name, param)
     where("#{field_name} like ?", "%#{param}%")
   end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
 end
